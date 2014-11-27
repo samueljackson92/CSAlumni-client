16da8f69569bc43e72e547d2cc8852c6e2d1f96c
Samuel Jackson <samueljackson@outlook.com>
2014-11-27 14:53:06 +0000

    Refs #3 Update fixtures to better mirror database.

--- tests/csa_api_test.py
+++ tests/csa_api_test.py
@@ -82,24 +82,27 @@ class ApiTests(unittest.TestCase):
     @responses.activate
     def test_connect_with_HTTP_auth(self):
         responses.add(responses.GET,
-                      self.api._build_end_point_uri('/users/show/:id', {':id': '42'}),
-                      body=load_fixture('users/show/42.json'),
+                      self.api._build_end_point_uri('/users/show/:id',
+                                                    {':id': '41'}),
+                      body=load_fixture('users/show/41.json'),
                       status=200,
                       content_type='application/json')
 
         self.api = CsaAPI("admin", "taliesin")
-        resp = self.api.make_request("/users/show/:id", {':id':'42'})
-        nose.tools.assert_equal("Jackson", resp["surname"])
+        resp = self.api.make_request("/users/show/:id", {':id':'41'})
+        nose.tools.assert_equal("Loftus", resp["surname"])
 
     @responses.activate
     @nose.tools.raises(requests.exceptions.HTTPError)
     def test_fail_to_connect_with_HTTP_auth(self):
         responses.add(responses.GET,
-                      self.api._build_end_point_uri('/users/show/:id', {':id': '42'}),
+                      self.api._build_end_point_uri('/users/show/:id',
+                                                    {':id': '41'}),
                       body='',
                       status=403,
                       content_type='application/json')
 
         #connect using an invalid password
         self.api = CsaAPI("admin", "not_a_password")
-        resp = self.api.make_request("/users/show/:id", {':id':'42'})
+        resp = self.api.make_request("/users/show/:id", {':id':'41'})
+
